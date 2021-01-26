1936b1d381cb4876796618343c3cbe12e0e67f33
lindeluhuang <linde.lu.huang@gmail.com>
2021-01-26 13:20:27 -0600

    adding intro text and styling

--- js/app.js
+++ js/app.js
@@ -1,3 +1,34 @@
-const Hello = props => <h1>Hello {props.name}</h1>;
+const Highlight = (props) => (
+  <span className={`relative highlight highlight-${props.color}`}>
+    <span className="relative z-2">{props.children}</span>
+  </span>
+);
 
-ReactDOM.render(<Hello name="Lawrence" />, document.getElementById('root'));
+const Intro = () => (
+  <div className="m-auto-ns f4 f3-m f2-l tc w-80-l normal">
+    <div className="mb3 mb4-ns">
+      <Highlight color="aqua">Lost in Tokyo</Highlight> is a directory of fun places to see, play in
+      and <Highlight color="yellow">explore</Highlight>, in{' '}
+      <Highlight color="blue">Tokyo</Highlight>, Japan.
+    </div>
+    <div>
+      From <Highlight color="blue">museums</Highlight> and{' '}
+      <Highlight color="blue">galleries</Highlight>, to{' '}
+      <Highlight color="pink">robot restaurants</Highlight> and{' '}
+      <Highlight color="pink">kitten cafes</Highlight>, Tokyo is the gift that keeps on giving.{' '}
+      <Highlight color="yellow">Dattebayo!</Highlight>
+    </div>
+  </div>
+);
+
+const App = () => (
+  <div>
+    <div className="min-vh-100 ph4 flex flex-column">
+      {/* our navigation component */}
+      <Intro />
+    </div>
+    <div className="flex flex-wrap container">{/* our attractions list component */}</div>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
