8e8713c9b5743dd29318d5d1fbe6191876a746d6
lindeluhuang <linde.lu.huang@gmail.com>
2021-01-27 16:42:28 -0600

    added image overlays and mouse events

--- js/app.js
+++ js/app.js
@@ -39,21 +39,51 @@ const Intro = () => (
   </div>
 );
 
+const Overlay = ({showInfo, title, description}) => (
+  <div
+    className="absolute w-100 h-100 flex items-center pa3 pa4-ns bg-aqua overlay"
+    style={{transform: showInfo ? 'none' : 'translateY(-100%)'}}
+  >
+    <div>
+      <h1 className="f4 f3-ns mt0 mb2 regular black normal lh-title">{title}</h1>
+      <p className="lh-title lh-copy-ns mv0 black f6 measure-l">{description}</p>
+    </div>
+  </div>
+);
+
 class Attraction extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showInfo: false,
+    };
+    this.toggleInfo = this.toggleInfo.bind(this);
+    this.closeInfo = this.closeInfo.bind(this);
+  }
+  toggleInfo() {
+    this.setState((prevState, props) => ({
+      showInfo: !prevState.showInfo,
+    }));
+  }
+
+  closeInfo() {
+    this.setState({
+      showInfo: false,
+    });
+  }
+
   render() {
     const {title, description, image, className} = this.props;
+    const {showInfo} = this.state;
     return (
       <div
         className={`ph4 ph5-ns ph0-l mb4 mb5-ns w-100 overflow-hidden pointer attraction ${className}`}
+        onClick={this.toggleInfo}
+        onMouseLeave={this.closeInfo}
       >
         <div className="relative">
-          <div className="absolute w-100 h-100 flex items-center pa3 pa4-ns bg-aqua overlay">
-            <div>
-              <h1 className="f4 f3-ns mt0 mb2 regular black normal lh-title">{title}</h1>
-              <p className="lh-title lh-copy-ns mv0 black f6 measure-l">{description}</p>
-            </div>
-          </div>
-          <img src={`../images/${image}`} />
+          <Overlay {...this.props} {...this.state} />
+          <img src={`../images/${image}`} className="db" />
         </div>
       </div>
     );
